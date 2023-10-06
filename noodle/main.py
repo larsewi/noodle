aba59bbeec97c9e2dafc0eb45811d31d632b1960
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-10-06 17:48:12 +0200

    Added program header
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- noodle/main.py
+++ noodle/main.py
@@ -2,10 +2,35 @@ import logging as log
 from arguments import parse_args
 
 
+NOODLE_HEADER = """
+                         _   _                 _ _
+                        | \ | |               | | |
+                        |  \| | ___   ___   __| | | ___
+                        | . ` |/ _ \ / _ \ / _` | |/ _ \\
+                        | |\  | (_) | (_) | (_| | |  __/
+                        |_| \_|\___/ \___/ \__,_|_|\___|
+
+                     *** Encryption not taken seriously ***
+
+ ------------------------------------------------------------------------------
+|                                                                              |
+|   Why did the Rebel Alliance hire a top-notch encryption expert to protect   |
+|   their data from the Empire?                                                |
+|                                                                              |
+|   Because they wanted to make sure their plans for defeating the Dark Side   |
+|   didn't end up on the Dark Web!                                             |
+|                                                                              |
+|   Remember, even in a galaxy far, far away, data security is essential!      |
+|                                                                              |
+ ------------------------------------------------------------------------------
+"""
+
 def main():
+    print(NOODLE_HEADER)
     args = parse_args()
     log.basicConfig(
         format="%(levelname)8s: %(message)s",
         level=log.DEBUG if args.debug else log.INFO,
     )
-    args.action(args)
+    if "action" in args:
+        args.action(args)
