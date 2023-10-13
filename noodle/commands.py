13a077a6c3938106547a84cd08e8c1a1bba69c8d
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-10-13 19:04:25 +0200

    Fix stdin
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- noodle/commands.py
+++ noodle/commands.py
@@ -18,22 +18,16 @@ def decrypt(filename=None):
         log.info("Aborted decryption: no file picked ...")
         return
 
-    if filename == "-":
-        input = sys.stdin.read()
-        process = subprocess.run(
-            ["gpg", "--decrypt"],
-            input=input,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            encoding="ascii",
-        )
-    else:
-        process = subprocess.run(
-            ["gpg", "--decrypt", filename],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            encoding="ascii",
-        )
+    args = ["gpg", "--decrypt"]
+    if filename != "-":
+        args.append(filename)
+
+    process = subprocess.run(
+        args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="ascii",
+    )
 
     if process.returncode != 0:
         log.error(f"Failed to encrypt {filename if filename != '-' else 'from stdin'}")
